b7e4ce7a3d0eda4af0fe0958a9cafa538b31de9b
ChaisedelEfrei <104134005+ChaisedelEfrei@users.noreply.github.com>
2023-10-09 04:11:52 +0200

    assignement 4

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port=3000;
 
 
 
+
 //first static endpoint static send index.hmtl file
 
 app.get('/',function(req,res){
@@ -45,3 +46,84 @@ app.get('/api/endpoint', (req, res) => {
 app.listen(port, function() {
     console.log('Server listening on http://localhost:' + port);
    });
+
+//Assignement 4:
+
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+// Initialize Knex instance
+const db = knex(knexConfig.development);
+
+app.use(express.json());
+
+// Create (Insert) Route
+app.post('/api/create', async (req, res) => {
+  try {
+    // Extract data from request body
+    const { title, test_id } = req.body;
+
+    // Insert a new record into the database
+    const [newRecord] = await db('test').insert({
+      title,
+      test_id
+    }).returning('title','id');
+    
+    res.json({ success: true, newRecord });
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: 'Error creating record.' });
+  }
+});
+
+// Read (Select) Route : search uan user with their pseudo
+app.get('/api/read/:param', async (req, res) => {
+  try {
+    const param = req.params.param;
+
+    // Query the database based on the parameter
+    const results = await db('users').where('username', 'like', `%${param}%`);
+
+    res.json({ success: true, results });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: 'Error fetching records.' });
+  }
+});
+
+// Update Route : the user table
+app.put('/api/update/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { username, email } = req.body;
+
+    // Update the record with the provided ID
+    await db('users').where('id', id).update({
+      username,
+      email,
+    });
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: 'Error updating record.' });
+  }
+});
+
+// Delete Route
+app.delete('/api/delete/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    // Delete the record with the provided ID
+    await db('test').where('test_id', id).del();
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: 'Error deleting record.' });
+  }
+});
+
+
---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      database: 'postgres',
+      user: 'postgres',
+      password: 'Chocolat14',
+    },
+  },
+};
